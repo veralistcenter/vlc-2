37f6ba2060ed6a836cf50e29883a85fbd30b12e0
Lukas Eigler-Harding <eiglerhardingl@gmail.com>
2022-07-27 21:43:00 +0200

    updated homepage query

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,25 @@
 <template>
   <main class="page">
-    
+    <pre>{{globals}}</pre>    
   </main>
 </template>
 
 <script>
+
+import { Global } from '@/services/Home'
+
 export default {
   name: 'IndexPage',
-  async asyncData({ $axios, $ReqWVars, $Req }){
-    
+  async asyncData({ $axios, $Req }){
+    const query = Global
+
+      try{
+        const res = await $axios($Req(query))
+        return { globals: res.data.data }
+
+      }catch(e){
+        return { globals: e }
+      }
   }
 }
 </script>
--- services/Home.js
+++ services/Home.js
@@ -1,1 +1,16 @@
 export const Home = ``
+
+export const Global = `acfOptions {
+  menu{
+    address
+    menuSocialLinks{
+      text
+      link
+    }
+  }
+  footer{
+    columnOne
+    columnTwo
+    columnThree
+  }
+}`
