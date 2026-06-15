6a2739fdcca09f2ab50b2da7bc734b96449cb612
veralistcenter <vlc@newschool.edu>
2026-06-15 13:51:41 +0300

    Add scroll to top button

--- pages/fellowships/index.vue
+++ pages/fellowships/index.vue
@@ -1,68 +1,77 @@
 <template>
-	<main class="page pt--8">
-		
-		<SiteSubnav 
-			:statement="'Statement String'"
-			:pages="pages"
-		/>
+  <main class="page pt--8">
+    <SiteSubnav :statement="'Statement String'" :pages="pages" />
 
-		<GridThumbs size="Half" :posts="fellowship.fellows.currentFellows.cfellows"  />
+    <GridThumbs
+      size="Half"
+      :posts="fellowship.fellows.currentFellows.cfellows"
+    />
 
-	</main>
+    <div class="section_inset mt--2">
+      <button
+        aria-label="Scroll to top"
+        class="btn--grey"
+        @click="scrollToTop()"
+      >
+        ↑ Scroll to top
+      </button>
+    </div>
+  </main>
 </template>
 
 <script>
-	
-	import { FellowPages } from '@/services/Fellowships'
-	import { mapGetters } from 'vuex'
+import { FellowPages } from "@/services/Fellowships";
+import { mapGetters } from "vuex";
 
-	export default{
-		head(){
-			return this.$metatags({title: 'Fellowships'})
-		},
+export default {
+  head() {
+    return this.$metatags({ title: "Fellowships" });
+  },
 
-		computed: {
-			fellowship(){
-				return this.settings.pages.edges.map(e => e.node).filter(p => p.slug === 'fellowships')[0]
-			},
-			...mapGetters({
-				settings: 'getSettings'
-			})
-		},
+  computed: {
+    fellowship() {
+      return this.settings.pages.edges
+        .map((e) => e.node)
+        .filter((p) => p.slug === "fellowships")[0];
+    },
+    ...mapGetters({
+      settings: "getSettings",
+    }),
+  },
 
-		async asyncData({ $axios, $Req, store }){
+  methods: {
+    scrollToTop() {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    },
+  },
 
+  async asyncData({ $axios, $Req, store }) {
+    try {
+      const query = FellowPages;
 
-			try{
+      const res = await $axios($Req(query));
 
-				const query = FellowPages
+      store.commit("updatePath", [
+        { title: "Home", route: "/" },
+        { title: "Fellowships", route: "/fellowships" },
+        { title: "Current", route: "/fellowships" },
+      ]);
 
-        const res = await $axios($Req(query))
-        
-        store.commit('updatePath', [
-        	{title: 'Home', route: '/'},
-        	{title: 'Fellowships', route: '/fellowships'},
-        	{title: 'Current', route: '/fellowships'}
-        ])
+      let pages = [
+        { title: "Current", path: "/fellowships" },
+        { title: "Past", path: "/fellowships/past" },
+      ];
 
-        let pages = [
-					{ title: 'Current', path: '/fellowships' }, 
-					{ title: 'Past', path: '/fellowships/past' },
-				]
+      const fpages = res.data.data.fellowships.edges.map((e) => {
+        return { title: e.node.title, path: `/fellowships/${e.node.slug}` };
+      });
 
-        const fpages = res.data.data.fellowships.edges.map(e => {
-        	return { title: e.node.title, path: `/fellowships/${e.node.slug}`}
-        })
-
-        return {
-					pages: [].concat(pages).concat(fpages)
-				}
-
-      }catch (e){
-      	return {error: e}
-      }
-
-		}
-	}
-
+      return {
+        pages: [].concat(pages).concat(fpages),
+      };
+    } catch (e) {
+      return { error: e };
+    }
+  },
+};
 </script>
--- pages/fellowships/past.vue
+++ pages/fellowships/past.vue
@@ -1,69 +1,77 @@
 <template>
-	<main class="page pt--8">
-		
-		<SiteSubnav 
-			:statement="'Statement String'"
-			:pages="pages"
-		/>
+  <main class="page pt--8">
+    <SiteSubnav :statement="'Statement String'" :pages="pages" />
 
-		<GridThumbs size="Quarter" :posts="fellowship.fellows.pastFellows.fellows"  />
+    <GridThumbs
+      size="Quarter"
+      :posts="fellowship.fellows.pastFellows.fellows"
+    />
 
-
-	</main>
+    <div class="section_inset mt--2">
+      <button
+        aria-label="Scroll to top"
+        class="btn--grey"
+        @click="scrollToTop()"
+      >
+        ↑ Scroll to top
+      </button>
+    </div>
+  </main>
 </template>
 
 <script>
-	
-	import { FellowPages } from '@/services/Fellowships'
-	import { mapGetters } from 'vuex'
+import { FellowPages } from "@/services/Fellowships";
+import { mapGetters } from "vuex";
 
-	export default{
-		head(){
-			return this.$metatags({title: 'Past Fellowships'})
-		},
+export default {
+  head() {
+    return this.$metatags({ title: "Past Fellowships" });
+  },
 
-		computed: {
-			fellowship(){
-				return this.settings.pages.edges.map(e => e.node).filter(p => p.slug === 'fellowships')[0]
-			},
-			...mapGetters({
-				settings: 'getSettings'
-			})
-		},
+  computed: {
+    fellowship() {
+      return this.settings.pages.edges
+        .map((e) => e.node)
+        .filter((p) => p.slug === "fellowships")[0];
+    },
+    ...mapGetters({
+      settings: "getSettings",
+    }),
+  },
 
-		async asyncData({ $axios, $Req, store }){
+  methods: {
+    scrollToTop() {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    },
+  },
 
+  async asyncData({ $axios, $Req, store }) {
+    try {
+      const query = FellowPages;
 
-			try{
+      const res = await $axios($Req(query));
 
-				const query = FellowPages
+      store.commit("updatePath", [
+        { title: "Home", route: "/" },
+        { title: "Fellowships", route: "/fellowships" },
+        { title: "Past", route: "/fellowships/past" },
+      ]);
 
-        const res = await $axios($Req(query))
-        
-        store.commit('updatePath', [
-        	{title: 'Home', route: '/'},
-        	{title: 'Fellowships', route: '/fellowships'},
-        	{title: 'Past', route: '/fellowships/past'}
-        ])
+      let pages = [
+        { title: "Current", path: "/fellowships" },
+        { title: "Past", path: "/fellowships/past" },
+      ];
 
-        let pages = [
-					{ title: 'Current', path: '/fellowships' }, 
-					{ title: 'Past', path: '/fellowships/past' },
-				]
+      const fpages = res.data.data.fellowships.edges.map((e) => {
+        return { title: e.node.title, path: `/fellowships/${e.node.slug}` };
+      });
 
-        const fpages = res.data.data.fellowships.edges.map(e => {
-        	return { title: e.node.title, path: `/fellowships/${e.node.slug}`}
-        })
-
-        return {
-					pages: [].concat(pages).concat(fpages)
-				}
-
-      }catch (e){
-      	return {error: e}
-      }
-
-		}
-	}
-
+      return {
+        pages: [].concat(pages).concat(fpages),
+      };
+    } catch (e) {
+      return { error: e };
+    }
+  },
+};
 </script>
