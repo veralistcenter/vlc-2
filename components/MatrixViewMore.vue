624e08b1506fa42c45010b4283449daf531d6f60
veralistcenter <vlc@newschool.edu>
2026-07-13 16:44:52 +0300

    Local styling

--- components/MatrixViewMore.vue
+++ components/MatrixViewMore.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="matrix_block mb--4 matrix_button no-print">
+  <section class="matrix_block mb--4 matrix_view_more no-print">
     <p class="fs--regular center caps">
       <button class="btn--full" @click="viewMore()">
         {{ block.title || "View More" }}
@@ -20,3 +20,19 @@ export default {
   },
 };
 </script>
+
+<style>
+.matrix_view_more {
+  position: relative;
+  margin-left: auto;
+  margin-right: auto;
+  width: 100%;
+  max-width: var(--max_width);
+}
+
+@media screen and (max-width: 768px) {
+  .matrix_view_more {
+    width: calc(100% - var(--margin_x2));
+  }
+}
+</style>
